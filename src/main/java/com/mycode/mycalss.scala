fa792f60ddf865611a05f722078b303e2ba62dbc
liuanb <liuanbo@qq.com>
2021-02-28 20:30:56 +0800

    second   commint

---
+++ src/main/java/com/mycode/mycalss.scala
@@ -0,0 +1,13 @@
+package com.mycode
+
+/**
+ * @author liuanbo
+ * @creat 2021-02-28-20:29
+ * @see 2194550857@qq.com
+ *
+ */
+object mycalss {
+  def main(args: Array[String]): Unit = {
+    println("fgh")
+  }
+}
